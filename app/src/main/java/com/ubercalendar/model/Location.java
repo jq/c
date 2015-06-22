31df0192e4b2bc66651b43fed61ad40ea91dc906
Qi Zhang <aileenzhang7@gmail.com>
2015-06-21 18:05:32 -0700

    add demo java files

---
+++ app/src/main/java/com/ubercalendar/model/Location.java
@@ -0,0 +1,36 @@
+package com.ubercalendar.model;
+
+/**
+ * Used by the user activity endpoint and contains information including the latitude, longitude &
+ * address of a location.
+ */
+public class Location extends UberModel {
+
+    /**
+     * Human-readable address.
+     */
+    String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * Latitude component of location.
+     */
+    double latitude;
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    /**
+     * Longitude component of location.
+     */
+    double longitude;
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+}
---
+++ app/src/main/java/com/ubercalendar/model/UberModel.java
@@ -0,0 +1,16 @@
+package com.ubercalendar.model;
+
+import com.ubercalendar.Constants;
+
+/**
+ * Helper class that overrides the {@link #toString()} method to print pretty json. All models
+ * extend this class.
+ */
+public class UberModel {
+
+    @Override
+    public String toString() {
+        return Constants.GSON.toJson(this);
+    }
+
+}
